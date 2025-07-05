6b6000e02cb58e84572b900e30fb50acc7aba017
lin6165288 <lin6165288@gmail.com>
2025-07-06 04:38:52 +0800

    Update customer_app.py

--- customer_app.py
+++ customer_app.py
@@ -25,6 +25,7 @@ if st.button("🔎 查詢"):
             conn = get_connection()
             sql = """
                 SELECT
+                  order_id        AS 訂單編號,
                   order_time      AS 下單日期,
                   platform        AS 平台,
                   tracking_number AS 單號,
